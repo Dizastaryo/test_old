8ed90b62fb4e00b759ea7e1954adea6e7776b8c3
Dizastaryo <tidi3855@gmail.com>
2026-01-29 01:01:33 +0500

    photo

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -13,7 +13,7 @@ class AppTheme {
         centerTitle: true,
         elevation: 0,
       ),
-      cardTheme: const CardThemeData(
+      cardTheme: const CardTheme(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(12)),
@@ -47,7 +47,7 @@ class AppTheme {
         centerTitle: true,
         elevation: 0,
       ),
-      cardTheme: const CardThemeData(
+      cardTheme: const CardTheme(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(12)),
